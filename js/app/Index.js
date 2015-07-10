f2f285759a4f41938f5badd542a2d3d9d7578613
Martin Hristov <tran4o@gmail.com>
2015-07-11 00:13:04 +0200

    sdas

--- js/app/Index.js
+++ js/app/Index.js
@@ -8,6 +8,23 @@ var Utils=require('./Utils');
 for (var e in Utils) 
 	window[e]=Utils[e];
 //---------------------------------------------------------------------------------------------------------
+function getSearchParameters() {
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+function transformToAssocArray( prmstr ) {
+  var params = {};
+  var prmarr = prmstr.split("&");
+  for ( var i = 0; i < prmarr.length; i++) {
+      var tmparr = prmarr[i].split("=");
+      params[tmparr[0]] = tmparr[1];
+  }
+  return params;
+}
+var params = getSearchParameters();
+//-----------------------------------------------
+console.warn(params);
+
 var tableFavorites=null;
 var tableParticipants=null;
 var tableRank=null;
