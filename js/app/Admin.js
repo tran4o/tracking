7d9db22a8367870334f99c1d045cf9cafb87f366
Martin Hristov <tran4o@gmail.com>
2015-08-16 15:23:48 +0300

    sd:x

--- js/app/Admin.js
+++ js/app/Admin.js
@@ -2,6 +2,9 @@ require('./Track');
 require('./GUI');
 require('./Participant');
 window.CONFIG=require('./Config');
+var Utils=require('./Utils');
+for (var e in Utils) 
+	window[e]=Utils[e];
 
 var draw;
 var modify;
