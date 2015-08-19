3bc2f757f110b8ad68fec2057a42677b128baf85
Martin Hristov <tran4o@gmail.com>
2015-08-19 12:48:09 +0300

    ds

--- js/app/Admin.js
+++ js/app/Admin.js
@@ -2,6 +2,7 @@ require('./Track');
 require('./GUI');
 require('./Participant');
 window.CONFIG=require('./Config');
+var STYLES=require('./Styles');
 var Utils=require('./Utils');
 for (var e in Utils) 
 	window[e]=Utils[e];
