7b4adbf9b7f83a0690484b9f5ccaeca6d52c9da9
Martin Hristov <tran4o@gmail.com>
2015-08-21 19:32:40 +0200

    sdasd

--- js/app/Config.js
+++ js/app/Config.js
@@ -34,7 +34,7 @@ var CONFIG =
 	math : {
 		gpsInaccuracy : 30,	//TODO 13 min
 		speedAndAccelerationAverageDegree : 2,	// calculation based on N states (average) (MIN 2)
-		displayDelay : 60,						// display delay in SECONDS
+		displayDelay : 80,						// display delay in SECONDS
 		interpolateGPSAverage : 0 // number of recent values to calculate average gps for position (smoothing the curve.min 0 = NO,1 = 2 values (current and last))
 	},
 	constants : 
