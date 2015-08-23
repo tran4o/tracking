aa777ad722293e7f25ee70e3cd3e2b4c272ba2ad
Martin Hristov <tran4o@gmail.com>
2015-08-23 10:38:37 +0300

    sdasd

--- js/app/Config.js
+++ js/app/Config.js
@@ -32,7 +32,7 @@ var CONFIG =
 	},
 	math : {
 		projectionScaleY : 0.75,				// TODO EXPLAIN (rectange creation in world mercator coef y 
-		gpsInaccuracy : 10,						 //TODO 13 min ? 
+		gpsInaccuracy : 30,						 //TODO 13 min ? 
 		speedAndAccelerationAverageDegree : 2,	// calculation based on N states (average) (MIN 2)
 		displayDelay : 80,						// display delay in SECONDS
 		interpolateGPSAverage : 0 // number of recent values to calculate average gps for position (smoothing the curve.min 0 = NO,1 = 2 values (current and last))
